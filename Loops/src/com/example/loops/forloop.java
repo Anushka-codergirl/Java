3ff1f93679822fce845b8d6000d70ef0736a24a5
Sunali Rambukwella <44220456+sunalii@users.noreply.github.com>
2020-10-03 19:05:36 +0530

    for loop from java

---
+++ Loops/src/com/example/loops/forloop.java
@@ -0,0 +1,9 @@
+package com.example.loops;
+
+public class ForLoop {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("This is a for loop");
+    }  
+  }
+}
