61ea6f16d20c393e71ee3acaefc896f40ca2735f
UmakanthSahu <69105946+UmakanthSahu@users.noreply.github.com>
2022-10-06 18:38:33 +0530

    Update Example1.java

--- Programs/InputOutput/Example1.java
+++ Programs/InputOutput/Example1.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Example1{
     public static void main(String[] args){
         double studentGPA = 3.45;
-		String studentFirstName = "Anushka";
-		String studentLastName = "Raj";
+	String studentFirstName = "Anushka";
+	String studentLastName = "Raj";
 
-		System.out.printf("%s %s has a GPA of %.2f.\n", studentFirstName, studentLastName, studentGPA);
-		System.out.println("Enter GPA you want to update it to:");
+	System.out.printf("%s %s has a GPA of %.2f.\n", studentFirstName, studentLastName, studentGPA);
+	System.out.println("Enter GPA you want to update it to:");
 
-		Scanner input = new Scanner(System.in);
-		studentGPA = input.nextDouble();
+	Scanner input = new Scanner(System.in);
+	studentGPA = input.nextDouble();
 
-		System.out.printf("%s %s now has a GPA of %.2f.\n", studentFirstName, studentLastName, studentGPA);
+	System.out.printf("%s %s now has a GPA of %.2f.\n", studentFirstName, studentLastName, studentGPA);
 
-		input.close();
+	input.close();
     }
 }
