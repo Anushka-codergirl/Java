8aec2ea0130e48952cb9eaa1ef52059d303c0a73
Wakanda Rajyam <116173131+wakandarajyam@users.noreply.github.com>
2022-10-19 19:08:10 +0530

    Improved Readability

--- Programs/JavaNIOAPI/CSVFile.java
+++ Programs/JavaNIOAPI/CSVFile.java
@@ -8,32 +8,31 @@ import java.util.stream.*;
 
 import model.Person;
 
-public class CSVFile{
-    public static void main(String[] args){
+public class CSVFile {
+	public static void main(String[] args) {
 
-        Path path = Paths.get("Files/data.csv");
+		Path path = Paths.get("Files/data.csv");
 
-        try(Stream<String> lines = Files.lines(path)){
-                        
-            lines.filter(line -> !line.startsWith("#")).flatMap(CSVFile::lineToPerson).forEach(System.out::println);
-            
-        }catch(IOException ioe){
-            ioe.printStackTrace();
-        }
-    }
+		try (Stream<String> lines = Files.lines(path)) {
 
-    private static Stream<Person> lineToPerson(String line){
-        try
-        {
-        String[] elements = line.split(";");
-        String name = elements[0];
-        int age = Integer.parseInt(elements[1]);
-        String city = elements[2];
+			lines.filter(line -> !line.startsWith("#")).flatMap(CSVFile::lineToPerson).forEach(System.out::println);
 
-        Person p = new Person(name, age, city);
-        return Stream.of(p);
-        }catch(Exception e){
-        }
-        return Stream.empty();        
-    }
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	private static Stream<Person> lineToPerson(String line) {
+		try {
+			String[] elements = line.split(";");
+			String name = elements[0];
+			int age = Integer.parseInt(elements[1]);
+			String city = elements[2];
+
+			Person p = new Person(name, age, city);
+			return Stream.of(p);
+		} catch (Exception e) {
+		}
+		return Stream.empty();
+	}
 }
--- Programs/JavaNIOAPI/WritingFiles.java
+++ Programs/JavaNIOAPI/WritingFiles.java
@@ -1,27 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.io.BufferedWriter;
-public class WritingFiles{
 
-    public static void main(String[] args) throws IOException {
+public class WritingFiles {
 
-        Path path = Paths.get("Files/SomeText.txt");
+	public static void main(String[] args) throws IOException {
 
-        try(  
-            BufferedWriter writer1 = Files.newBufferedWriter(path);
-            BufferedWriter writer2 = new BufferedWriter(writer1);
-            PrintWriter pW = new PrintWriter(writer2);
-        ){
-        writer1.write("Hello World");      
-    
-        pW.printf("\n I = %d\n", 10);
+		Path path = Paths.get("Files/SomeText.txt");
 
-    }catch(IOException ioe){
-        ioe.printStackTrace();
-    }
-    System.out.println("Done");
-    }
+		try (BufferedWriter writer1 = Files.newBufferedWriter(path);
+				BufferedWriter writer2 = new BufferedWriter(writer1);
+				PrintWriter pw = new PrintWriter(writer2);) {
+			
+			writer1.write("Hello World");
+
+			pw.printf("\n I = %d\n", 10);
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		System.out.println("Done");
+	}
 }
