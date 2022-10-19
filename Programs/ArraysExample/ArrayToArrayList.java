844a701c7ebb37428fb6033fe13d6ad3acfd1344
Wakanda Rajyam <116173131+wakandarajyam@users.noreply.github.com>
2022-10-19 18:51:41 +0530

    improved Readability

--- Programs/ArraysExample/ArrayToArrayList.java
+++ Programs/ArraysExample/ArrayToArrayList.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayToArrayList {
-   public static void main(String args[]) {
-      String[] array = {"a", "b", "c", "d", "e"};
+	public static void main(String args[]) {
+		String[] alphabets= { "a", "b", "c", "d", "e" };
 
-      //Create a new list. Iterate the array and add each element to the list.
-      ArrayList<String> list = new ArrayList<String>();
-      for(String text:array) {
-         list.add(text);
-      }
-      System.out.println(list);
-   }  
+		// Create a new list. Iterate the array and add each element to the list.
+		List<String> list = new ArrayList<String>();
+		for (String alphabet : alphabets) {
+			list.add(alphabet);
+		}
+		System.out.println(list);
+	}
 }
-
